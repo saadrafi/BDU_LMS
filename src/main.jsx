7c6ce756df90ebad68c9b81a4f173bbfe949d9a1
saadrafi <saadrafi81@gmail.com>
2024-01-26 19:48:32 +0600

    initial setup

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div>
+      <h1 className='text-7xl text-center'>Hello, world!</h1>
+    </div>
+  </React.StrictMode>,
+)
